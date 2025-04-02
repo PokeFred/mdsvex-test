42414c7930077d041ef19f1c192f58c79def499d
PokeFred <Cedric.paelmke@gmx.de>
2025-04-02 13:27:54 +0200

    init svelte

---
+++ svelte.config.js
@@ -0,0 +1,32 @@
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            pages: "./build",
+            assets: "./build",
+            fallback: undefined,
+            precompress: true,
+            strict: true,
+        }),
+        env: {
+            publicPrefix: "PUBLIC_",
+            privatePrefix: "PRIVATE_"
+        },
+        alias: {
+            $server: "./src/lib/server",
+            $components: "./src/lib/components",
+            $layouts: "./src/lib/layouts",
+            $utils: "./src/lib/utils",
+            $images: "./src/lib/images",
+            $stores: "./src/lib/stores",
+            $styles: "./src/lib/styles",
+            $types: "./src/lib/types"
+        }
+    }
+}
+
+export default config
